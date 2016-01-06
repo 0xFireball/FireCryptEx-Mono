8bd5740693e67792e540becd5a4f99f3e9392ad5
0xFireball <0xFireball@WindowsBuildServer.local>
2016-01-06 15:28:27 -0800

    update assemblyinfo

--- FireCrypt/FireCrypt/Properties/AssemblyInfo.cs
+++ FireCrypt/FireCrypt/Properties/AssemblyInfo.cs
@@ -1,31 +1,29 @@
-﻿#region Using directives
-
+#region Using directives
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 #endregion
-
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FireCrypt")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FireCrypt")]
-[assembly: AssemblyCopyright("Copyright 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle ("FireCrypt")]
+[assembly: AssemblyDescription ("FireCrypt - Free encryption software to create secure encrypted volumes.")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("0xFireball, AluminumDev")]
+[assembly: AssemblyProduct ("FireCrypt")]
+[assembly: AssemblyCopyright ("Copyright 2015-2016, 0xFireball. All Rights Reserved.")]
+[assembly: AssemblyTrademark ("FireCrypt")]
+[assembly: AssemblyCulture ("")]
 // This sets the default COM visibility of types in the assembly to invisible.
 // If you need to expose a type to COM, use [ComVisible(true)] on that type.
-[assembly: ComVisible(false)]
-
+[assembly: ComVisible (false)]
 // The assembly version has following format :
 //
 // Major.Minor.Build.Revision
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.1.*")]
+[assembly: AssemblyVersion ("1.2.1.*")]
+[assembly: AssemblyFileVersion ("1.2.1")]
+[assembly: Guid ("6598eed9-9fb6-4d89-a61a-e8d35306bf6b")]
